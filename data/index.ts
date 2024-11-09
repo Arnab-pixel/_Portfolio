130c3a209ccc51c22ab9530ba2668f9b1bb75698
Arnab-pixel <arnabchaudhuri2002@gmail.com>
2024-11-09 18:30:51 +0530

    Update

--- data/index.ts
+++ data/index.ts
@@ -49,7 +49,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building MERN Dashboard App",
+    title: "Currently building StoreIt a Storage app",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
